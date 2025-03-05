63e13f59777f829c07fa182b6f40ef583d9a8ff4
betolink <betolin@gmail.com>
2025-03-05 17:44:47 -0600

    refactored bulk ingest, still not uploading file to s3

--- cryoforge/__init__.py
+++ cryoforge/__init__.py
@@ -1,8 +1,6 @@
 from .generate import generate_itslive_metadata, save_metadata, create_stac_item
 from .ingestitem import ingest_item, ingest_stac
 from .ingestbulk import ingest_items
-from .ingestcoiled import ingest_stac_coiled
-from .ingestcoiledv2 import ingest_stac_coiled_async
 
 __all__ = [
     "generate_itslive_metadata",
@@ -11,6 +9,4 @@ __all__ = [
     "ingest_item",
     "ingest_stac",
     "ingest_items",
-    "ingest_stac_coiled",
-    "ingest_stac_coiled_async"
 ]
--- cryoforge/ingestbulk.py
+++ cryoforge/ingestbulk.py
@@ -6,11 +6,15 @@ import argparse
 import logging
 import dask
 import fsspec
+import os
+import orjson
 from dask.diagnostics.progress import ProgressBar
 from .generate import generate_itslive_metadata
 
 import requests
 import ctypes
+import pyarrow as pa
+import stac_geoparquet
 
 def trim_memory() -> int:
     libc = ctypes.CDLL("libc.so.6")
@@ -33,30 +37,59 @@ def post_or_put(url: str, data: dict):
 
 def generate_stac_metadata(url: str, stac_server: str, collection: str):
     stack_metadata = generate_itslive_metadata(url)
-    stac_item = stack_metadata["stac"].to_dict()
+    stac_item = stack_metadata["stac"]
     try:
-        post_or_put(urljoin(stac_server, f"collections/{collection}/items"), stac_item)
+        post_or_put(urljoin(stac_server, f"collections/{collection}/items"), stac_item.to_dict())
     except Exception as e:
         logging.error(f"Error with {url}: {e}")
-    return
+    return stac_item
     
-def ingest_items(list_file: str, stac_server: str, scheduler: str ="processes", workers: int = 4):
+def ingest_items(list_file: str,
+                 stac_server: str,
+                 scheduler: str ="processes",
+                 workers: int = 4,
+                 format: str = "json",
+                 s3_bucket: str = "",
+                 ingest: bool = False):
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(message)s",
         datefmt="%m/%d/%Y %I:%M:%S %p",
         level=logging.INFO,
     )
+    task_id = int(os.environ["COILED_BATCH_TASK_ID", "-1"])
+    if task_id>=0:
+        # coiled job
+        list_files = s3.glob(path_list + "/**/*.txt")
+        if task_id >= len(list_files):
+            logging.info(f"Task ID {task_id} is out of range")
+            return
+        current_page = list_files[task_id]
+    else:
+        current_page = list_file
+    current_page_name = current_page.split("/")[-1].replace(".txt", "")
 
-    with fsspec.open(list_file, mode="rt") as f:
+    with fsspec.open(current_page, mode="rt") as f:
         urls = f.read().splitlines()
 
     logging.info(f"Reading list from {list_file}, {len(urls)} URLs found.")
     tasks = [dask.delayed(generate_stac_metadata)(url, stac_server, "itslive") for url in urls]
     with ProgressBar():
-        results = dask.compute(*tasks,
+        features = dask.compute(*tasks,
                                scheduler=scheduler,
                                num_workers=int(workers))
+        trim_memory()
+    logging.info(f"Finished processing {current_page}, {len(features)} STAC items generated.")
 
+    if format == "json":
+        with open(f"{current_page_name}.ndjson", mode="wb") as f:
+            for item in features:
+                f.write(orjson.dumps(item.to_dict()) + b"\n")
+    elif format == "parquet":
+        items_arrow = stac_geoparquet.arrow.parse_stac_items_to_arrow(features)
+        stac_geoparquet.arrow.to_parquet(items_arrow, f"{current_page_name}.parquet")
+    else:
+        logging.error(f"Invalid format {format}")
+       
 
 def ingest_stac():
     """Ingest sample data during docker-compose"""
@@ -68,13 +101,19 @@ def ingest_stac():
     )
     parser.add_argument("-w", "--workers", type=int, default=4, help="Number of workers")
     parser.add_argument("-s", "--scheduler", default="processes", help="Dask scheduler")
-    
-    parser.add_argument("-t", "--target", required=True, help="STAC endpoint")
+    parser.add_argument("-f", "--format", default="json", help="STAC serialization, json or parquet")
+    parser.add_argument("-b", "--bucket", default="s3://its-live-data/test-space/luis_catalogs/sentinel1/stac", help="S3 path where the output should be upload to")
+    parser.add_argument("-i", "--ingest", help="If present the stac items will be ingested into the STAC endpoint")
+    parser.add_argument("-t", "--target", help="STAC endpoint where items will be ingested")
 
     args = parser.parse_args()
 
-    stac_endpoint = args.target
-    ingest_items(list_file=args.list, stac_server=stac_endpoint, scheduler=args.scheduler, workers=args.workers)
+    ingest_items(list_file=args.list,
+                 stac_server=args.target,
+                 scheduler=args.scheduler,
+                 workers=args.workers,
+                 s3_bucket=args.bucket,
+                 ingest=args.ingest)
 
 
 if __name__ == "__main__":
