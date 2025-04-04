a4613eb43eea519760dee52f46227bfec6a66995
betolink <betolin@gmail.com>
2025-04-04 15:25:42 -0500

    fix search

--- cryoforge/search_items.py
+++ cryoforge/search_items.py
@@ -23,17 +23,15 @@ def get_bbox_wgs84(nc_url):
 
     return [lon_min, lat_min, lon_max, lat_max]
 
-def search_stac(bbox, max_items=100, percent_valid_pixels=None):
-    stac_url = "https://stac.itslive.cloud/"
-    catalog = Client.open(stac_url)
-    print(f"Searching STAC catalog with bbox: {bbox}")
-
+def search_stac(stac_catalog, bbox, max_items=100, percent_valid_pixels=None):
+    catalog = Client.open(stac_catalog)
     search_kwargs = {
         "collections": ["itslive-granules"],
         "bbox": bbox,
         "max_items": max_items
     }
 
+    # TODO: add more filters and flexibility 
     if percent_valid_pixels is not None:
         search_kwargs["filter"] = {
             "op": ">=",
@@ -42,7 +40,6 @@ def search_stac(bbox, max_items=100, percent_valid_pixels=None):
         search_kwargs["filter_lang"] = "cql2-json"
 
     search = catalog.search(**search_kwargs)
-    print("\nSearch Results:")
     
     hrefs = []
     for item in search.items():
@@ -54,8 +51,9 @@ def search_stac(bbox, max_items=100, percent_valid_pixels=None):
 
 def search_items():
     parser = argparse.ArgumentParser(description="Search STAC catalog based on bounding box derived from a NetCDF file.")
+    parser.add_argument("--catalog", help="URL of the STAC catalog")
     parser.add_argument("--granule", help="URL of the NetCDF file")
-    parser.add_argument("--bbox", required=True, help="Bounding box in the format 'lon_min,lat_min,lon_max,lat_max'")
+    parser.add_argument("--bbox", help="Bounding box in the format 'lon_min,lat_min,lon_max,lat_max'")
     parser.add_argument("--max-items", type=int, default=100, help="Maximum number of items to return (default: 100)")
     parser.add_argument("--percent-valid-pixels", type=int, help="Filter items by minimum percent valid pixels (e.g., 90)")
     args = parser.parse_args()
@@ -69,8 +67,13 @@ def search_items():
                 raise ValueError("Bounding box must contain exactly four values.")
         else:
             raise ValueError("Either --granule or --bbox must be provided.")
-        results = search_stac(bbox, args.max_items, args.percent_valid_pixels)
-        print(json.dumps(results, indent=2))
+        if args.catalog:
+            catalog = args.catalog
+        else:
+            catalog = "https://stac.itslive.cloud/"
+        results = search_stac(catalog, bbox, args.max_items, args.percent_valid_pixels)
+        for href in results:
+            print(href)
     except Exception as e:
         print(f"Error: {e}", file=sys.stderr)
         sys.exit(1)
